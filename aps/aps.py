c742ee9b18d442561990e67bc494b588e4cff195
isohaven <ridgehaven320@gmail.com>
2018-05-21 10:12:21 -0500

    added restore

--- aps/aps.py
+++ aps/aps.py
@@ -32,6 +32,18 @@ y_test = make_one_hot(y_test)
 print('parsing done')
 
 
+def saveModel (sess):
+    saver = tf.train.Saver()
+    saver.save(sess, 'C:/Users/ridge/Desktop/machinelearning/aps/model/model.ckpt')
+
+def restoreModel (sess):
+    saver = tf.train.Saver()
+    saver.restore(sess, 'C:/Users/ridge/Desktop/machinelearning/aps/model/model.ckpt')
+    merged = tf.summary.merge_all()
+    writer = tf.summary.FileWriter('scrap15', sess.graph)
+
+
+
 
 class dnn:
     def __init__(
@@ -77,6 +89,7 @@ class dnn:
         layer = tf.add(tf.matmul(prev_layer, weights), biases)
         return layer
 
+
     def train(self, featureset):
         model = self.model(featureset)
         cost_function = tf.reduce_mean(
@@ -84,27 +97,39 @@ class dnn:
         optimizer = tf.train.AdamOptimizer(self.learning_rate).minimize(cost_function)
 
 
-        init = tf.global_variables_initializer()
 
         with tf.Session() as sess:
-            sess.run(init)
+            # saver = tf.train.Saver()
+
+            sess.run(tf.global_variables_initializer()) # comment this line if restoring model
+            # restoreModel(sess)
+
 
             for i in range(1, self.n_epochs+1):
                 avg_cost = 0
-
-
                 sess.run(optimizer, feed_dict = {self.x: X_train, self.y: y_train})
-
                 avg_cost += sess.run(cost_function, feed_dict={self.x: X_train, self.y: y_train})
 
-            print('epoch', i, 'complete')
+                print('epoch', i, 'of' , self.n_epochs, 'complete')
             predictions = tf.equal(tf.argmax(model, 1), tf.argmax(self.y, 1))
             accuracy = tf.reduce_mean(tf.cast(predictions, 'float'))
+            if i % 20 == 0:
+                saveModel(sess)
+
+
+            # data = [59816,-9999,1010,936,0,0,0,0,0,0,123922,984314,1680050,1135268,92606,14038,1772828,0,0,0,1116,2372,3546760,3053176,652616,423374,0,0,7274,0,1622,432,0,0,0,0,0,6388,1091104,2930694,2012,0,3526,904,1426,223226,2663348,1137664,104,0,0,0,1283806,928212,345132,265930,194770,158262,219942,620264,13880,0,4201350,98,238,880,16,1772828,51468,331744,316130,176000,208420,159380,-9999,-999,-99999,-9999,-9999,100120,59816.46,4201350,4201350,4203050,29967.0,26214,51894,562680,4030198,1209600,114684,0,144,0,3387773.76,38633.28,599624.64,0,0,0,0,14308,475410,1109740,1528024,837114,58942,6220,440,1278,1292,4201350,4,6846,810,70090,345884,191284,2454600,926846,33558,280,0,1516,1398,2050280,64066,0,674,0,46,3413978,2924,414,0,0,60,38710,0,0,0,0,27740,33354,6330,0,0,133542,21290,2718360,435370,0,0,0,0,1179900,1541.32,1678,659550,691580,540820,243270,483302,485332,431376,210074,281662,3232,0,0]
+            # pred = sess.run(model, feed_dict={self.x: np.expand_dims(data, 0)})
+
             print('accuracy:', accuracy.eval({self.x : X_test, self.y: y_test}))
+            # saver.save(sess, 'C:/Users/ridge/Desktop/machinelearning/aps/model/model.ckpt')
+            
+
+   
+
         
 
-d = dnn(170, 2)
+d = dnn(170, 2, n_epochs=50)
 x = d.x
 print('training')
 d.train(x)
-print('yeet')
+print('done')
