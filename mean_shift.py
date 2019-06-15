bd9cba2cb4f2eae3c316236fa3537f7ec110ada6
isohaven <ridgehaven320@gmail.com>
2019-06-15 16:23:50 -0400

    implemented kernel, maybe

--- mean_shift.py
+++ mean_shift.py
@@ -5,18 +5,18 @@ style.use('ggplot')
 
 fig, ax  = plt.subplots()
 
+norm = lambda x1, x2: np.linalg.norm(x1-x2)
 def draw_circle(x=0, y=0, r=1):
     circle = plt.Circle( (x, y), r, alpha=0.35)
     ax.add_artist(circle)
 def neighbors(point, X, distance):
     friends = [] 
     for x in X:
-        if( np.linalg.norm(point-x) <= distance):
+        if( norm(point, x) <= distance):
             friends.append(x)
     return friends
-def kernel(point1, point2):
-    distance = np.linalg.norm(point1-point2)
-    return k(p1- p2)
+def kernel(distance, bandwidth):
+    return np.exp( -1*np.square(distance) / ( 2* (bandwidth**2) ) ) 
 xs = np.arange(1, 51) # xs [1-50]
 ys = 50 * np.random.random((1, 50))[0] # ys randoms [0-1]
 X = np.column_stack((xs, ys)) # X is featureset [xs, ys]
