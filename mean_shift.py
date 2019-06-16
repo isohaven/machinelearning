1941fe45ff463a92c3ad2103962097e65e7bce9c
isohaven <ridgehaven320@gmail.com>
2019-06-15 22:08:49 -0400

    changes

--- mean_shift.py
+++ mean_shift.py
@@ -17,13 +17,12 @@ def neighbors(point, X, distance):
     return friends
 def kernel(distance, bandwidth):
     return np.exp( -1*np.square(distance) / ( 2* (bandwidth**2) ) ) 
+
+
 xs = np.arange(1, 51) # xs [1-50]
 ys = 50 * np.random.random((1, 50))[0] # ys randoms [0-1]
 X = np.column_stack((xs, ys)) # X is featureset [xs, ys]
 # print(X)
-draw_circle(15, 15, 7)
 plt.scatter(X[:,0], X[:, 1], s=100)
-plt.scatter(15, 15, c='b')
-print(neighbors( [15, 15], X, 7))
 plt.show()
 
